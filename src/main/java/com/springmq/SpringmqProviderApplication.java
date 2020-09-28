b228c765e88e9f10479f7061785c58186b78a9f3
hhy666 <h18835005270@163.com>
2020-09-28 16:17:26 +0800

    rebbitmq运用

---
+++ src/main/java/com/springmq/SpringmqProviderApplication.java
@@ -0,0 +1,13 @@
+package com.springmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmqProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmqProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springmq/SpringmqProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmqProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
